a24c6dae29ddfee367cabdae5dc9dd47670cc5cb
Martin Asser Hansen <mail@maasha.dk>
2017-03-18 22:39:49 +0100

    Finished header component

--- app/components/header.component.js
+++ app/components/header.component.js
@@ -4,7 +4,9 @@ class Header extends React.Component {
   render () {
     return (
       <div className='header'>
-        <h1>Header!</h1>
+        <button type='button' onClick={this.openMenu}>{'\u2630'}</button>
+        <button type='button' onClick={this.getHelp}>?</button>
+        <button type='button' onClick={this.exitApp}>X</button>
       </div>
     )
   }
