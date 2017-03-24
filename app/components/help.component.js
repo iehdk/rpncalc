4a5519c174e64f51df1c18081204027f287ddddb
Martin Asser Hansen <mail@maasha.dk>
2017-03-24 23:30:20 +0100

    Added some help text

--- app/components/help.component.js
+++ app/components/help.component.js
@@ -13,7 +13,17 @@ class Help extends React.Component {
         >
           <button type='button' onClick={this.props.closeHelp}>X</button>
           <h2 ref='subtitle'>Help</h2>
-          <p>Help text here</p>
+          <p>Reverse Polish notation (RPN) is a method for representing
+             expressions in which the operator symbol is placed after the
+             arguments being operated on.
+          </p>
+          <p>
+            For example, the following RPN expression will produce the sum of 2
+            and 3, namely 5: 2 3 +.
+          </p>
+          <p>
+            Read more about RPN <a href='http://www.calculator.org/rpn.aspx'>here</a>.
+          </p>
         </Modal>
       </div>
     )
