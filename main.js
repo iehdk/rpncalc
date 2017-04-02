9dd89ea0f5cfae147ea2b6dbc2dd1a09e581d20a
Martin Asser Hansen <mail@maasha.dk>
2017-04-02 20:43:38 +0200

    adding windows package

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 
 require('electron-reload')(__dirname)
 
+//handle setupevents as quickly as possible
+const setupEvents = require('./installers/setupEvents')
+
+if (setupEvents.handleSquirrelEvent()) {
+   // squirrel event handled and app will exit in 1000ms, so don't do anything else
+   return
+}
+
 const electron = require('electron')
 
 // Module to control application life.
