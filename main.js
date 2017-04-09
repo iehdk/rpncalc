c935904545a1f94f87713ce1713d0c1fe924160b
Martin Asser Hansen <mail@maasha.dk>
2017-04-09 15:28:29 +0200

    removed body margin

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 375, height: 400, frame: true})
+  mainWindow = new BrowserWindow({width: 360, height: 378, frame: true})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
