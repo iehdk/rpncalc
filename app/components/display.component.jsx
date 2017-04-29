78d418efed3cb8a6a30c74355e85bc8cac6fc9b4
Martin Asser Hansen <mail@maasha.dk>
2017-04-29 20:02:45 +0200

    relinting display component

---
+++ app/components/display.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends React.Component {
+  componentDidMount() {
+    const node = document.getElementById('display');
+    node.scrollTop = node.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    const node = document.getElementById('display');
+    node.scrollTop = node.scrollHeight;
+  }
+
+  render() {
+    return (
+      <div
+        className="display"
+      >
+        <textarea
+          readOnly
+          id="display"
+          rows={this.props.rows}
+          cols={this.props.cols}
+          value={this.props.stack.render()}
+        />
+      </div>
+    );
+  }
+}
+
+Display.propTypes = {
+  rows: PropTypes.number,
+  cols: PropTypes.number,
+  stack: PropTypes.string,
+};
+
+Display.defaultProps = {
+  rows: 4,
+  cols: 30,
+  stack: 'fobar',
+};
+
+export default Display;
