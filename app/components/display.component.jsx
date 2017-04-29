c251d9599754fad9776d485f3b701559dc28b99b
Martin Asser Hansen <mail@maasha.dk>
2017-04-29 23:07:44 +0200

    documented display component

--- app/components/display.component.jsx
+++ app/components/display.component.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Container for rendering the calculater display which is where the stack, or
+ * list of entered and calculated numbers, is displayed.
+ * @type {[type]}
+ */
 class Display extends React.Component {
+  /**
+   * React life cycle method we use to scroll to the bottom of the display when
+   * the component mounts.
+   */
   componentDidMount() {
     const node = document.getElementById('display');
     node.scrollTop = node.scrollHeight;
   }
 
+  /**
+   * React life cycle method we use to scroll to the bottom of the display when
+   * the component updates.
+   */
   componentDidUpdate() {
     const node = document.getElementById('display');
     node.scrollTop = node.scrollHeight;
   }
 
+  /**
+   * React.component's required render method for rendering the display.
+   * @return {Object} JSX code that is transpiled to Javascript.
+   */
   render() {
     return (
       <div
