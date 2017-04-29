3686ffb45ed5c6eed7de60f117a59bb5fce71c6d
Martin Asser Hansen <mail@maasha.dk>
2017-04-29 20:36:19 +0200

    re-linting key-swap component

--- app/components/key-swap.component.jsx
+++ app/components/key-swap.component.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class KeySwap extends React.Component {
-  render () {
-    const tip = 'Swap the last two values on the stack'
+function KeySwap(props) {
+  const tip = 'Swap the last two values on the stack';
 
-    return (
-      <button
-        data-tip={tip}
-        className='key key-width-2'
-        type='button'
-        value='swap'
-        onClick={this.props.handleOnClick}
-      >
-        SWAP
-      </button>
-    )
-  }
+  return (
+    <button
+      data-tip={tip}
+      className="key key-width-2"
+      type="button"
+      value="swap"
+      onClick={props.handleOnClick}
+    >
+      SWAP
+    </button>
+  );
 }
 
-export default KeySwap
+KeySwap.propTypes = {
+  handleOnClick: PropTypes.func,
+};
+
+export default KeySwap;
