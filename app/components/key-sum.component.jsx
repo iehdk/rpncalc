be1350a8d62e7be84cc27135ce23450be30900c6
Martin Asser Hansen <mail@maasha.dk>
2017-04-29 20:34:39 +0200

    re-linting key-sum component

--- app/components/key-sum.component.jsx
+++ app/components/key-sum.component.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class KeySum extends React.Component {
-  render () {
-    const tip = 'Sum all values on the stack'
+function KeySum(props) {
+  const tip = 'Sum all values on the stack';
 
-    return (
-      <button
-        data-tip={tip}
-        className='key key-width-1'
-        type='button'
-        value='sum'
-        onClick={this.props.handleOnClick}
-      >
-        {'\u03a3'}
-      </button>
-    )
-  }
+  return (
+    <button
+      data-tip={tip}
+      className="key key-width-1"
+      type="button"
+      value="sum"
+      onClick={props.handleOnClick}
+    >
+      {'\u03a3'}
+    </button>
+  );
 }
 
-export default KeySum
+KeySum.propTypes = {
+  handleOnClick: PropTypes.func,
+};
+
+export default KeySum;
