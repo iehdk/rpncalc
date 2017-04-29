c630dc1d5ca8844b29e63887a86cdcfc6f6d9295
Martin Asser Hansen <mail@maasha.dk>
2017-04-29 09:40:25 +0200

    Corrected typo

--- app/components/key-exp.component.js
+++ app/components/key-exp.component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class KeyExp extends React.Component {
   render () {
-    const tip = 'Calculate the exponent' +
+    const tip = 'Calculate the exponent ' +
                 'of the last two elements ' +
                 'on the stack where: ' +
                 '2: x and 1: y'
