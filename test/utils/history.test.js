fb37ec18f520e5aef48b0b1687da316b0c62c590
Martin Asser Hansen <mail@maasha.dk>
2017-05-04 21:57:28 +0200

    Added tests for History constructor

---
+++ test/utils/history.test.js
@@ -0,0 +1,30 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import History from '../../app/utils/history.util';
+
+describe('History', () => {
+  const history = new History();
+
+  describe('Constructor', () => {
+    it('should have one property', () => {
+      expect(Object.keys(history).length).to.eql(1);
+    });
+
+    it('should have property ary', () => {
+      expect(history).to.have.property('ary');
+    });
+
+    describe('without parameter', () => {
+      it('should have empty ary', () => {
+        expect(history.ary).to.eql([]);
+      });
+    });
+
+    describe('with parameter [[2], [2, 5]]', () => {
+      const history2 = new History([[2], [2, 5]]);
+      it('should have ary [[2], [2, 5]]', () => {
+        expect(history2.ary).to.eql([[2], [2, 5]]);
+      });
+    });
+  });
+});
