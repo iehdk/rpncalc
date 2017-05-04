1257c46d2aaf4f2fdbbf1189aabc6bc1b87aa06b
Martin Asser Hansen <mail@maasha.dk>
2017-05-04 21:51:40 +0200

    Stack constructor tests passing

--- test/utils/stack.test.js
+++ test/utils/stack.test.js
@@ -2,17 +2,29 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import Stack from '../../app/utils/stack.util';
 
-describe('a passing test', () => {
-  it('should pass', () => {
-    expect(true).to.be.true;
-  });
-});
-
 describe('Stack', () => {
+  const stack = new Stack();
+
   describe('Constructor', () => {
-    it('should have one property, ary', () => {
-      const sut = new Stack();
-      expect(sut).to.have.property('ary'); // should pass
+    it('should have one property', () => {
+      expect(Object.keys(stack).length).to.eql(1);
+    });
+
+    it('should have property ary', () => {
+      expect(stack).to.have.property('ary');
+    });
+
+    describe('without parameter', () => {
+      it('should have empty ary', () => {
+        expect(stack.ary).to.eql([]);
+      });
+    });
+
+    describe('with parameter [2, 5]', () => {
+      const stack2 = new Stack([2, 5]);
+      it('should have ary [2, 5]', () => {
+        expect(stack2.ary).to.eql([2, 5]);
+      });
     });
   });
 });
