83be101a5c194ff2f38ed4e0b9fead8de61cafba
Martin Asser Hansen <mail@maasha.dk>
2017-05-08 23:05:07 +0200

    Adding test for History

--- app/utils/history.util.js
+++ app/utils/history.util.js
@@ -25,35 +25,38 @@ class History {
   /**
    * Constructor for History.
    * @param {Array} history History array.
+   * @param {Number} maxLength Maximum length of history ary.
    */
-  constructor(history) {
+  constructor(history, maxLength) {
     /**
      * Array for storing a list of stacks each representing a historic state.
      * @type {Array}
      */
     this.ary = history || [];
+    this.maxLength = maxLength || MAX_LENGTH;
   }
 
   /**
-   * Return last element on the history array. Return null if no elements.
+   * Return last element on the history array or an empty array if empty
+   * history.
    * @return {Object} History array element.
    */
   last() {
     if (this.ary.length === 0) {
-      return null;
+      return [];
     }
 
     return this.ary[this.ary.length - 1];
   }
 
   /**
-   * Pop and return the last element from the history array. Return undef if no
-   * elements.
+   * Pop and return the last element from the history array. Return empty array
+   * if no elements.
    * @return {Object} History array element.
    */
   pop() {
     if (this.ary.length === 0) {
-      return null;
+      return [];
     }
 
     return this.ary.pop();
@@ -67,7 +70,7 @@ class History {
   push(elem) {
     this.ary.push(elem);
 
-    if (this.ary.length > MAX_LENGTH) {
+    if (this.ary.length > this.maxLength) {
       this.ary.shift();
     }
 
@@ -82,7 +85,7 @@ class History {
     const file = historyFilePath();
 
     if (!fs.existsSync(file)) {
-      return null;
+      return this;
     }
 
     const data = fs.readFileSync(file, 'utf8');
@@ -99,7 +102,7 @@ class History {
    */
   save() {
     if (this.ary.length === 0) {
-      return null;
+      return this;
     }
 
     const json = JSON.stringify(this.ary, null, 2);
--- test/utils/history.test.js
+++ test/utils/history.test.js
@@ -2,21 +2,34 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import History from '../../app/utils/history.util';
 
-describe('history.util -> History Class', () => {
-  const history = new History();
+/**
+ * Max number of elements on the history array.
+ * @type {Number}
+ */
+const MAX_LENGTH = 50;
 
+describe('history.util -> History Class', () => {
   describe('constructor', () => {
-    it('should have one property', () => {
-      expect(Object.keys(history).length).to.eql(1);
+    const history = new History();
+    it('should have two properties', () => {
+      expect(Object.keys(history).length).to.eql(2);
     });
 
     it('should have property ary', () => {
       expect(history).to.have.property('ary');
     });
 
+    it('should have property maxLength', () => {
+      expect(history).to.have.property('maxLength');
+    });
+
     describe('without parameter', () => {
       it('should have empty ary', () => {
         expect(history.ary).to.eql([]);
+      });
+
+      it('should have default maxLength', () => {
+        expect(history.maxLength).to.eql(MAX_LENGTH);
       });
     });
 
@@ -26,5 +39,112 @@ describe('history.util -> History Class', () => {
         expect(history2.ary).to.eql([[2], [2, 5]]);
       });
     });
+
+    describe('with parameters [[2], [2, 5]], 7', () => {
+      const history2 = new History([[2], [2, 5]], 7);
+      it('should have ary [[2], [2, 5]]', () => {
+        expect(history2.ary).to.eql([[2], [2, 5]]);
+      });
+
+      it('should have maxLength 7', () => {
+        expect(history2.maxLength).to.eql(7);
+      });
+    });
+  });
+
+  describe('last', () => {
+    describe('with empty history', () => {
+      const history = new History();
+      it('should return an empty ary', () => {
+        expect(history.last()).to.eql([]);
+      });
+    });
+
+    describe('with one event history', () => {
+      const history = new History([[1]]);
+      it('should return the last ary', () => {
+        expect(history.last()).to.eql([1]);
+      });
+    });
+
+    describe('with two event history', () => {
+      const history = new History([[1], [1, 2]]);
+      it('should return the last ary', () => {
+        expect(history.last()).to.eql([1, 2]);
+      });
+    });
+  });
+
+  describe('pop', () => {
+    describe('with empty history', () => {
+      const history = new History();
+      it('should return an empty ary', () => {
+        expect(history.pop()).to.eql([]);
+      });
+    });
+
+    describe('with one event history', () => {
+      const history = new History([[1]]);
+      it('should return the last ary', () => {
+        expect(history.pop()).to.eql([1]);
+      });
+
+      it('should leave an empty history ary', () => {
+        expect(history.last()).to.eql([]);
+      });
+    });
+
+    describe('with two event history', () => {
+      const history = new History([[1], [1, 2]]);
+      it('should return the last ary', () => {
+        expect(history.pop()).to.eql([1, 2]);
+      });
+
+      it('should leave the first element on the stack', () => {
+        expect(history.last()).to.eql([1]);
+      });
+    });
+  });
+
+  describe('push', () => {
+    describe('with empty history', () => {
+      const history = new History();
+      it('should add new element', () => {
+        expect(history.push([1]).last()).to.eql([1]);
+      });
+    });
+
+    describe('with one event history', () => {
+      const history = new History([1]);
+      it('should add new element', () => {
+        expect(history.push([2]).last()).to.eql([2]);
+      });
+    });
+
+    describe('with maxLength 2', () => {
+      const history = new History([1], 2);
+      history.push([1, 2]);
+      history.push([3]);
+      history.push([5, 6]);
+      it('should have 2 elements', () => {
+        expect(history.ary.length).to.eql(2);
+      });
+
+      it('should have last element [5, 6]', () => {
+        expect(history.last()).to.eql([5, 6]);
+      });
+
+      it('should have first element [3]', () => {
+        expect(history.ary[0]).to.eql([3]);
+      });
+    });
+  });
+
+  describe('load', () => {
+
+  });
+
+  describe('save', () => {
+
   });
 });
