0e9107888bc577a3a58d11c772c3804bbd177d6b
Martin Asser Hansen <mail@maasha.dk>
2017-06-02 09:47:50 +0200

    Added tests for prompt event handlers

--- test/components/prompt.test.jsx
+++ test/components/prompt.test.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { beforeEach, describe, it } from 'mocha';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import Prompt from '../../app/components/prompt.component';
 
@@ -20,5 +21,19 @@ describe('<Prompt />', () => {
 
   it('should have one input field', () => {
     expect(wrapper.find('input')).to.have.length(1);
+  });
+
+  it('should call _handleOnChange when typing', () => {
+    const spy = sinon.spy();
+    const wrapper2 = shallow(<Prompt _handleOnChange={spy} />);
+    wrapper2.find('input').simulate('change', { target: { value: 'abc' } });
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('should call _handleOnSubmit on submit', () => {
+    const spy = sinon.spy();
+    const wrapper2 = shallow(<Prompt _handleOnSubmit={spy} />);
+    wrapper2.find('form').simulate('submit');
+    expect(spy.calledOnce).to.be.true;
   });
 });
